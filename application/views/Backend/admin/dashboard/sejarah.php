289ec3322f88d3d047f0dae4bdd608c6a2ec7b14
unknown <rendifauzan0987@gmail.com>
2024-02-15 23:22:29 +0700

    update

--- application/views/Backend/admin/dashboard/sejarah.php
+++ application/views/Backend/admin/dashboard/sejarah.php
@@ -73,31 +73,9 @@
        </div>
        <div class="card-body">
          <p class="card-text p-typo">
-           <center><b>• SEJARAH</b></br>
-             <p align="justify"> &nbsp;&nbsp;&nbsp;PT. Global Printpack Indonesia begerak dibidang perkebunan kelapa sawit dengan luas areal pencadangan
-               ± 15.000 hektar dengan pabrik pengelolahan minyak kelapa sawit (PMKS) yang berkapasitas maksimum 90 ton
-               tbs / jam. secara geografis PT.Suryabumi Agrolanggeng terletak di Tujuh belas (17) desa yaitu Desa Hidup baru,
-               Desa Padang Bindu, Desa Betung, Desa Pagar Dewa, Desa Pagar Jati, Desa Rami Pasai, Desa Penangiran,
-               Desa Gunung Megang Luar, Desa Lubuk Mumpo, Desa Talang Bulang Selatan, Desa Talang Bulang, Desa Berugo Darat,
-               Desa Benuang, Desa Kartadewa, Desa Jerambah Besi, Desa Simpang Tais, Desa Simpang Raja dan berada ditiga kecamatan
-               yaitu Kecamatan benakat, Kecamatan Gunung Megang dan kecamatan Talang Ubi serta terletak di dua kabupaten yaitu
-               Kabupaten Muara enim dan Kabupaten penukal abab Lematang ilir provinsi sumatra selatan. perusahaan ini mulai
-               melakukan penanaman kelapa sawit pada tahun 1992 dengan surat keputusan (SK) pencadangan dari guburnur
-               nomor791/i/1991 tertanggal 12 november 1991 diterbitkan hak guna usaha (HGU) seluas 8.384,78 hektar pada bulan desember
-               tahun 2000 dengan nomor 01/hgu/12/2000. <br><br>
-               &nbsp;&nbsp;&nbsp; Luas area yang digunakan untuk pembangunan pengolahan minyak kelapa sawit
-               dengan kapasitas 90 ton tbs / jam dengan bangunan terdiri dari pabrik, kantor, labolatorium, bengkel
-               dan perumahan / mess karyawan adalah seluas 13.7 hektar. Untuk saat ini pabrik sudah beroprasi 90 ton perjam.
-               pada saat ini jumlah karyawan bulanan berjumlah 525 orang yang tediri staff dan non staff sedangkan karyawan non staff
-               berjumlah 650 orang diluar tenaga kerja outsourcing (pihak ketiga) yang berjumlah lebih kurang 200 orang.
-               Jam keja 7.30 wib s/d 16.30 istirahat 2 (dua) jam perhari atau 40 (empat puluh) jam seminggu. kelapa sawit di perkenalkan
-               pertama sekali diindonesia oleh pemerintah kolonial belanda pada tahun 1848. ketika itu ada empat bibit kelapa sawit
-               yang dibawa oleh mauritius dari ansterdam dan ditanam kebun raya bogor. tanaman kelapa sawit mulai dibudidayakan secara
-               komersial pada tahun 1911 di aceh dan di sumatra utara oleh adrien hallet, seorang berkebangsaan belgia.
-               luas kebun kelapa sawit terus bertambah dari tahun ketahun perkebunan kelapa sawit pertama berlokasi
-               di pantai timur sumatra (deli) dan aceh. luas areal perkebunannya mencapai 5.123 ha. indonesia mulai
-               mengakspor minyak sawit pada tahun 1919 sebesar 576 ton kenegara-negara eropa, kemudia pada tahun 1923 mulai
-               mengekspor minyak inti kelapa sawit sebesar 850 ton.</p>
+          <center><h1><b>SEJARAH</b></h1></center>
+             <h3 align="justify"> <br>PT. Global Printpack Indonesia (GPI) is privately owned. The founder of this company has been experience in rotogravure and aseptic packaging industry for 20 years. </br>
+              
            </center></br>
 
 
